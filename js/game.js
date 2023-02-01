19bd856ccf5982653563d306b64e227ee63aa7ad
joselinecedano <joselinecedano0@gmail.com>
2023-02-01 17:21:02 -0500

    Class ctreated with shuffle and flipCards functions

--- js/game.js
+++ js/game.js
@@ -3,99 +3,56 @@ console.log("Poke-Match")
 //Query Selectors
 let startBtn = document.querySelector(".start")
 let gameArea = document.querySelector(".grid")
+let timer = document.querySelector(".time")
+
+
+//Game class
 class PokeMatch {
     constructor(){
-        this.cards = document.querySelectorAll(".card")
+        this.cards = document.querySelectorAll(".card");
+        this.firstPick = false ;
+        this.firstPokemon = '';
+        this.secondPick = false;
+        this.secondPokemon = '';
     }
-    matches(){
-
-
-    }
-}
-
-
-let newGame = new PokeMatch
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//this stores what the first and second card user chooses in order to check if they matched 
-// let firstPick;
-// let secondPick;
-// //Function that checks for matched cards
-// const checkMatch = () => {
-//     for (let i = 0; i < cards.length; i++){
-//         if(){ //set firstPick to the card user selected 
-//         firstPick = cards[i]
-//         }else{
-//             secondPick= cards[i]
-//         }
-//     if(firstPick.id === secondPick.id){
-//         console.log("Its a match!")
-        
-//     }else {
-//     console.log("They weren't a match!")
-// }
-
-// }}
-// checkMatch()
-//Function that flips cards 
-// const flip = () => {
-//     for (let i = 0; i < cards.length; i++){
-//         do{
-//             cards[i].classList.toggle("flip")
-//         }while(cards[i].id !== cards[i].id)
-//         if(cards[i].id === cards[i].id){
-//             cards[i].remove("flip")
-//         }
-// }}
-//Function that unflips cards if they're not a match
-   
-//Function for Winner 
-
-//Function for Loser
-
-//Function for Timer
-
-//Function for Resetting the Game
-
-//Function that Randomizes/Shuffles Cards
-
-
-//Event Listeners
-//Event listener for each card that adds flip class 
- newGame.cards.forEach(card => 
-    card.addEventListener("click", ()=>{
-        card.classList.toggle('flip');
-        console.log()
-    }
- ))
-startBtn.addEventListener("click", ()=>{
-    for (let i = 0; i < newGame.cards.length; i++){
-        let pokemonShuffle = newGame.cards[Math.floor(Math.random()*newGame.cards.length)];
+   shuffle(){
+    for (let i = 0; i < this.cards.length; i++){
+        let pokemonShuffle = this.cards[Math.floor(Math.random()*this.cards.length)];
+        // append it to the section that contains the grid so it shows the shuffled cards inside of the game browser
         gameArea.appendChild(pokemonShuffle)
         console.log(pokemonShuffle) 
-}})
+        }
+    }
+    flipCards(){
+        this.cards.forEach(card => {
+            let card = card.addEventListener("click",()=> {
+                card.classList.add(".flip")
+
+            })
+        })
+    }
+    checkForMatches(){
+        //if firstPick is false that means a card hasnt been picked yet and will run whats inside
+        if(!firstPick){
+            
+            console.log(firstPick, "first pick")
+        }
+ 
+    }
+    reset(){ //maybe?
+        //reset timer
+        // this.cards.classList.remove("flip")
+        // this.firstPick = false 
+        // this.firstPokemon = ''
+        // this.secondPick = false
+        // this.secondPokemon = ''
+    }
+     
+}
+let newGame = new PokeMatch
+newGame.shuffle();
+newGame.flipCards();
+newGame.checkForMatches()
+
+    
+    
